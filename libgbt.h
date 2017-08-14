07f59b39c0ebf248bcbb6c94aff77b373ef358fa
z3bra <contactatz3bradotorg>
2017-08-14 08:45:44 +0200

    Add torrent structure

--- libgbt.h
+++ libgbt.h
@@ -9,6 +9,20 @@ struct bdata {
 };
 TAILQ_HEAD(blist, bdata);
 
+struct torrent {
+	char *url;
+	uint8_t *bits;
+	struct file {
+		char *path;
+		size_t len;
+	} *files;
+	struct piece {
+		uint8_t sha1[20];
+		uint8_t *data;
+		size_t len;
+	} *pieces;
+};
+
 int bfree(struct blist *);
 struct blist * bdecode(char *, size_t);
 struct bdata * bsearchkey(struct blist *, const char *);
